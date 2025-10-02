91b93faebf0604d98049427b956cfef52bb632d6
william chavula <rumbani@gmail.com>
2025-10-02 14:50:00 -0500

    cleanup duplicate interface definitions

--- backend/src/db/interfaces/auth.ts
+++ backend/src/db/interfaces/auth.ts
@@ -1,16 +1,3 @@
-import { Request } from "express";
-import { SafeUser, UpdateUserProfile } from "../schema/users";
-import { RegisterData, LoginCredentials } from "../../services/auth.service";
-import { Session } from "../schema/sessions";
-import { ChangePasswordData, EmailData } from "./common";
-
-export interface AuthRequest extends Request {
-  userId?: number;
-  sessionId?: number;
-  user?: SafeUser;
-  sanitizedBody?: RegisterData | LoginCredentials | ChangePasswordData | RefreshTokenData | UpdateUserProfile | EmailData | ResetPasswordData;
-}
-
 export interface RefreshTokenData {
   refreshToken: string;
 }
@@ -26,5 +13,3 @@ export interface TokenPayload {
   iat?: number;
   exp?: number;
 }
-
-export interface AuthSession extends Session {}
