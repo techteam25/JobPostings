6c6280731b61597a418c9f2d6968dcbf2385ee9f
william chavula <rumbani@gmail.com>
2025-10-08 08:24:09 -0500

    ensure redis connection before initializing RedisStore

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,10 +1,15 @@
 import { createClient } from "redis";
 import { env } from "@/config/env";
 import { RedisStore } from "rate-limit-redis";
+import logger from "@/logger";
 
 const redisClient = createClient({
   url: env.REDIS_URL,
 });
+
+redisClient.on("error", (err) => logger.error(err, "Redis Client Error"));
+
+redisClient.connect().catch(logger.error);
 
 export const store = new RedisStore({
   sendCommand: (...args: string[]) => redisClient.sendCommand(args),
