5319bf50edc1f128ef1acf4925c33a473b58a3a4
william chavula <rumbani@gmail.com>
2025-10-09 11:45:13 -0500

    update searchParams validation to use coercion and add additional query parameters

--- backend/src/validations/base.validation.ts
+++ backend/src/validations/base.validation.ts
@@ -3,10 +3,18 @@ import { z } from "zod";
 export const searchParams = z.object({
   body: z.object({}).strict(),
   query: z.object({
-    page: z.string().optional().default("1").transform(Number).optional(),
-    limit: z.string().optional().default("10").transform(Number).optional(),
+    page: z.coerce.number().optional().default(1).transform(Number).optional(),
+    limit: z.coerce
+      .number()
+      .optional()
+      .default(10)
+      .transform(Number)
+      .optional(),
+    q: z.string().optional(),
+    jobType: z.string().optional(),
+    location: z.string().optional(),
+    isRemote: z.coerce.boolean().optional(),
     sortBy: z.string().optional(),
-    search: z.string().optional(),
     status: z
       .enum([
         "pending",
