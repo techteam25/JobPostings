3077b88d82e6046ac1c50796ae1182fcf522d901
william chavula <rumbani@gmail.com>
2025-10-10 15:16:47 -0500

    feat(jobInsights.repository): implement error handling for job insights database operations

--- backend/src/repositories/jobInsights.repository.ts
+++ backend/src/repositories/jobInsights.repository.ts
@@ -2,6 +2,7 @@ import { BaseRepository } from "./base.repository";
 import { jobInsights } from "@/db/schema";
 import { db } from "@/db/connection";
 import { eq, sql } from "drizzle-orm";
+import { withDbErrorHandling } from "@/db/dbErrorHandler";
 
 export class JobInsightsRepository extends BaseRepository<typeof jobInsights> {
   constructor() {
@@ -10,35 +11,44 @@ export class JobInsightsRepository extends BaseRepository<typeof jobInsights> {
   async getJobInsights() {}
 
   async incrementJobViews(jobId: number) {
-    await db
-      .update(jobInsights)
-      .set({
-        viewCount: sql`(${jobInsights.viewCount} + 1)`,
-      })
-      .where(eq(jobInsights.job, jobId));
+    await withDbErrorHandling(
+      async () =>
+        await db
+          .update(jobInsights)
+          .set({
+            viewCount: sql`(${jobInsights.viewCount} + 1)`,
+          })
+          .where(eq(jobInsights.job, jobId)),
+    );
   }
   async incrementJobApplications(jobId: number) {
-    await db
-      .update(jobInsights)
-      .set({
-        viewCount: sql`(${jobInsights.applicationCount} + 1)`,
-      })
-      .where(eq(jobInsights.job, jobId));
+    await withDbErrorHandling(
+      async () =>
+        await db
+          .update(jobInsights)
+          .set({
+            viewCount: sql`(${jobInsights.applicationCount} + 1)`,
+          })
+          .where(eq(jobInsights.job, jobId)),
+    );
   }
 
   async getJobInsightByOrganizationId(organizationId: number) {
-    const [result] = await db
-      .select()
-      .from(jobInsights)
-      .where(eq(jobInsights.organization, organizationId));
+    const [result] = await withDbErrorHandling(
+      async () =>
+        await db
+          .select()
+          .from(jobInsights)
+          .where(eq(jobInsights.organization, organizationId)),
+    );
     return result;
   }
 
   async getJobInsightByJobId(jobId: number) {
-    const [result] = await db
-      .select()
-      .from(jobInsights)
-      .where(eq(jobInsights.job, jobId));
+    const [result] = await withDbErrorHandling(
+      async () =>
+        await db.select().from(jobInsights).where(eq(jobInsights.job, jobId)),
+    );
     return result;
   }
 }
