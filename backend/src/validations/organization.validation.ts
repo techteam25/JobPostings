2f4b392360914fd03168721dc99c36e7bdfabb31
william chavula <rumbani@gmail.com>
2025-10-10 19:40:46 -0500

    feat(organization.validation): update Zod import to use custom swagger registry

--- backend/src/validations/organization.validation.ts
+++ backend/src/validations/organization.validation.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from "@/swagger/registry";
 
 const organizationPayloadSchema = z.object({
   name: z.string().min(5, "Name must be at least 5 characters").max(100),
