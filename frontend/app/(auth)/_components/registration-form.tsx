10c6e90e98ce289d72b1757be7bf0e6ac7f05d55
william chavula <rumbani@gmail.com>
2025-10-16 14:45:51 -0500

    feat(RegistrationForm): enhance registration form with new icons for user types and social login options

--- frontend/app/(auth)/_components/registration-form.tsx
+++ frontend/app/(auth)/_components/registration-form.tsx
@@ -3,11 +3,16 @@
 import { useState } from "react";
 import { Label } from "@/components/ui/label";
 
-import { Eye, EyeOff } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
+import { MdWork } from "react-icons/md";
 
 export default function RegistrationForm() {
   const [showPassword, setShowPassword] = useState(false);
@@ -36,13 +41,15 @@ export default function RegistrationForm() {
               className={cn(
                 "cursor-pointer rounded-2xl border-2 p-4 transition",
                 {
-                  "border-chart-4 bg-background": accountType === "jobseeker",
+                  "border-chart-4 bg-background": accountType === "user",
                   "border-secondary hover:border-border":
-                    accountType !== "jobseeker",
+                    accountType !== "user",
                 },
               )}
             >
-              <div className="mb-2 text-3xl">👤</div>
+              <div className="mb-2 flex justify-center text-4xl">
+                <BsFillPersonFill className="text-chart-1" />
+              </div>
               <div className="text-foreground font-semibold">Job Seeker</div>
               <div className="text-muted-foreground text-xs">
                 Looking for opportunities
@@ -60,7 +67,9 @@ export default function RegistrationForm() {
                 },
               )}
             >
-              <div className="mb-2 text-3xl">💼</div>
+              <div className="mb-2 flex justify-center text-4xl">
+                <MdWork className="text-chart-2" />
+              </div>
               <div className="text-foreground font-semibold">Employer</div>
               <div className="text-muted-foreground text-xs">Hiring talent</div>
             </button>
@@ -127,9 +136,9 @@ export default function RegistrationForm() {
               className="hover:text-secondary-foreground text-secondary-foreground absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer border-none bg-transparent shadow-none hover:bg-transparent"
             >
               {showPassword ? (
-                <EyeOff className="size-6" />
+                <BsEyeSlash className="size-6" />
               ) : (
-                <Eye className="size-6" />
+                <BsEye className="size-6" />
               )}
             </Button>
           </div>
@@ -156,9 +165,9 @@ export default function RegistrationForm() {
               className="hover:text-secondary-foreground text-secondary-foreground absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer border-none bg-transparent shadow-none hover:bg-transparent"
             >
               {showConfirmPassword ? (
-                <EyeOff className="size-5" />
+                <BsEyeSlash className="size-5" />
               ) : (
-                <Eye className="size-5" />
+                <BsEye className="size-5" />
               )}
             </Button>
           </div>
@@ -206,33 +215,14 @@ export default function RegistrationForm() {
             type="button"
             className="border-border flex items-center justify-center gap-2 rounded-lg border px-4 py-3 transition hover:bg-gray-50"
           >
-            <svg className="h-5 w-5" viewBox="0 0 24 24">
-              <path
-                fill="#4285F4"
-                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-              />
-              <path
-                fill="#34A853"
-                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-              />
-              <path
-                fill="#FBBC05"
-                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-              />
-              <path
-                fill="#EA4335"
-                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-              />
-            </svg>
+            <FcGoogle className="size-6" />
             Google
           </button>
           <button
             type="button"
             className="border-border flex items-center justify-center gap-2 rounded-lg border px-4 py-3 transition hover:bg-gray-50"
           >
-            <svg className="h-5 w-5" fill="#0A66C2" viewBox="0 0 24 24">
-              <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-            </svg>
+            <BsLinkedin className="size-6 text-[#0072b1]" />
             LinkedIn
           </button>
         </div>
