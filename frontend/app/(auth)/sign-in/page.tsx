6fd086cbe262ed119802dd86c234d795e82c7649
william chavula <rumbani@gmail.com>
2025-10-17 10:17:33 -0500

    feat: styles for page layout

--- frontend/app/(auth)/sign-in/page.tsx
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import LoginForm from "@/app/(auth)/_components/login-form";
+
 function Page() {
-  return <div>SignIn Page</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4 py-8">
+      <LoginForm />
+    </div>
+  );
 }
 
 export default Page;
