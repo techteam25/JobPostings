6b2310b1a5f4f8f65e1efc6886fbb512dbdb530e
william chavula <rumbani@gmail.com>
2025-10-16 21:35:50 -0500

    feat: configure environment variables for client and server URLs

--- frontend/env.ts
+++ frontend/env.ts
@@ -2,10 +2,12 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    SERVER_URL: z.url(),
+  server: {},
+  client: {
+    NEXT_PUBLIC_SERVER_URL: z.url(),
   },
-  client: {},
   // For Next.js >= 13.4.4, you only need to destructure client variables:
-  experimental__runtimeEnv: {},
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
 });
