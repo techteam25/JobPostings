b639bca3eeefba4b63dbe3c2443b0384208b10bb
william chavula <rumbani@gmail.com>
2025-10-22 22:10:48 -0500

    feat: invalidate user session query on sign out success

--- frontend/app/(main)/hooks/use-user-signout.ts
+++ frontend/app/(main)/hooks/use-user-signout.ts
@@ -1,16 +1,18 @@
 import { redirect, RedirectType } from "next/navigation";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { toast } from "sonner";
 
 import { authClient } from "@/lib/auth";
 
 export const useUserSignOut = () => {
+  const queryClient = useQueryClient();
   const { mutateAsync: signOutAsyncAction, isPending } = useMutation({
     mutationFn: async () =>
       await authClient.signOut({
         fetchOptions: {
           onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["get-user-session"] });
             toast.success("Signed out successfully");
             redirect("/sign-in", RedirectType.replace);
           },
