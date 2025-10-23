dcba9b3f387e3e8c63dd61bd82a326a1d419a556
william chavula <rumbani@gmail.com>
2025-10-23 18:34:17 -0500

    chore: add typesense client configuration

---
+++ backend/src/config/typesense-client.ts
@@ -0,0 +1,15 @@
+import Typesense from "typesense";
+
+import { env } from "./env";
+
+export const typesenseClient = new Typesense.Client({
+  nodes: [
+    {
+      host: env.TYPESENSE_HOST,
+      port: env.TYPESENSE_PORT,
+      protocol: env.TYPESENSE_PROTOCOL,
+    },
+  ],
+  apiKey: env.TYPESENSE_API_KEY,
+  connectionTimeoutSeconds: 2,
+});
