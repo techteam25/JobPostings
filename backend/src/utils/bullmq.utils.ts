b57988def5e6ed221aa95aa8489debfb92b09c24
william chavula <rumbani@gmail.com>
2025-10-24 13:11:00 -0500

    chore: implement BullMQ job indexing and email sending workers

---
+++ backend/src/utils/bullmq.utils.ts
@@ -0,0 +1,111 @@
+import { Queue, QueueEvents, Worker, Job as BullMqJob } from "bullmq";
+
+import logger from "@/logger";
+
+import { env } from "@/config/env";
+import { JobWithSkills } from "@/validations/job.validation";
+import { TypesenseService } from "@/services/typesense.service/typesense.service";
+
+const typesenseService = new TypesenseService();
+
+const jobIndexerQueueEvents = new QueueEvents("jobIndexQueue", {
+  connection: { url: env.REDIS_URL },
+});
+const emailSenderQueueEvents = new QueueEvents("emailQueue", {
+  connection: { url: env.REDIS_URL },
+});
+
+export const jobIndexerQueue = new Queue("jobIndexQueue", {
+  connection: {
+    url: env.REDIS_URL,
+  },
+  defaultJobOptions: {
+    removeOnComplete: true,
+    removeOnFail: 500,
+    attempts: 3,
+    backoff: {
+      type: "exponential",
+      delay: 1000,
+    },
+  },
+});
+
+export const emailSenderQueue = new Queue("emailQueue", {
+  connection: {
+    url: env.REDIS_URL,
+  },
+  defaultJobOptions: {
+    removeOnComplete: true,
+    removeOnFail: 500,
+    attempts: 5,
+    backoff: {
+      type: "exponential",
+      delay: 1000,
+    },
+  },
+});
+
+const jobIndexerWorker = new Worker<JobWithSkills>(
+  "jobIndexQueue",
+  async (job: BullMqJob) => {
+    const jobData = job.data as JobWithSkills;
+
+    if (job.name === "indexJob") {
+      await typesenseService.indexJobDocument(jobData);
+    }
+    if (job.name === "updateJobIndex") {
+      await typesenseService.updateJobDocumentById(`${jobData.id}`, jobData);
+    }
+    if (job.name === "deleteJobIndex") {
+      await typesenseService.deleteJobDocumentById(`${jobData.id}`);
+    }
+  },
+  {
+    connection: {
+      url: env.REDIS_URL,
+    },
+    removeOnComplete: { count: 100 },
+    removeOnFail: { count: 500 },
+  },
+);
+
+const emailSenderWorker = new Worker(
+  "emailQueue",
+  async (job: BullMqJob) => {},
+  {
+    connection: {
+      url: env.REDIS_URL,
+    },
+    removeOnComplete: { count: 100 },
+    removeOnFail: { count: 500 },
+  },
+);
+
+jobIndexerQueueEvents.on("completed", () => {
+  // update statistics
+});
+
+emailSenderQueueEvents.on("completed", () => {
+  // update statistics
+});
+
+jobIndexerQueueEvents.on("failed", ({ jobId, failedReason }) => {
+  // update statistics
+  logger.error(`Job Indexer Job ${jobId} failed: ${failedReason}`);
+});
+
+emailSenderQueueEvents.on("failed", ({ jobId, failedReason }) => {
+  // update statistics
+  logger.error(`Email Sender Job ${jobId} failed: ${failedReason}`);
+});
+
+jobIndexerWorker.on("error", (err) => {
+  logger.error(err);
+});
+
+emailSenderWorker.on("error", (err) => {
+  logger.error(err);
+});
+
+jobIndexerWorker.run().catch(logger.error);
+emailSenderWorker.run().catch(logger.error);
