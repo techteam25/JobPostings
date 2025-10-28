18fea7753c9f0af0d4b286443a8bca032de9cb07
william chavula <rumbani@gmail.com>
2025-10-27 22:21:27 -0500

    feat: add methods to attach and retrieve notes for job applications

--- backend/src/controllers/organization.controller.ts
+++ backend/src/controllers/organization.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { OrganizationService } from "@/services/organization.service";
 import { BaseController } from "./base.controller";
 import {
+  CreateJobApplicationNoteInputSchema,
   CreateOrganizationSchema,
   DeleteOrganizationSchema,
   GetOrganizationSchema,
@@ -15,6 +16,7 @@ import {
   Organization,
   OrganizationMember,
 } from "@/validations/organization.validation";
+import { JobApplicationWithNotes } from "@/validations/jobApplications.validation";
 
 export class OrganizationController extends BaseController {
   private organizationService: OrganizationService;
@@ -244,6 +246,76 @@ export class OrganizationController extends BaseController {
         success: false,
         status: "error",
         message: "Failed to update job application status",
+        error: (error as Error).message,
+        timestamp: new Date().toISOString(),
+      });
+    }
+  };
+
+  attachNoteToJobApplication = async (
+    req: Request<
+      JobApplicationManagementSchema["params"],
+      {},
+      CreateJobApplicationNoteInputSchema["body"]
+    >,
+    res: Response<ApiResponse<JobApplicationWithNotes>>,
+  ) => {
+    const applicationId = parseInt(req.params.applicationId);
+    const jobId = parseInt(req.params.jobId);
+
+    const note = req.body;
+
+    try {
+      const result = await this.organizationService.createJobApplicationNote(
+        applicationId,
+        jobId,
+        note,
+      );
+
+      return res.status(201).json({
+        success: true,
+        data: result,
+        message: "Job application note created successfully",
+        timestamp: new Date().toISOString(),
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        status: "error",
+        message: "Failed to create job application note",
+        error: (error as Error).message,
+        timestamp: new Date().toISOString(),
+      });
+    }
+  };
+
+  getNotesForJobApplication = async (
+    req: Request<JobApplicationManagementSchema["params"]>,
+    res: Response<ApiResponse<{ note: string; createdAt: Date }[]>>,
+  ) => {
+    const organizationId = parseInt(req.params.organizationId);
+    const applicationId = parseInt(req.params.applicationId);
+    const jobId = parseInt(req.params.jobId);
+
+    try {
+      const applicationNotes =
+        await this.organizationService.getNotesForJobApplication(
+          organizationId,
+          jobId,
+          applicationId,
+        );
+
+      return res.json({
+        success: true,
+        message: "Job application notes retrieved successfully",
+        data: applicationNotes,
+        timestamp: new Date().toISOString(),
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        status: "error",
+        message: "Failed to retrieve job application notes",
         error: (error as Error).message,
         timestamp: new Date().toISOString(),
       });
