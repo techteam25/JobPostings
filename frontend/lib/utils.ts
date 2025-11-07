9316da3545e1fc5ccb67656223ff6d5e48fc5b76
william chavula <rumbani@gmail.com>
2025-11-07 15:43:54 -0600

    feat: add utility function to format posted dates

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDistance, subDays } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPostedDate(date: Date) {
+  return formatDistance(subDays(new Date(date), 0), new Date(), {
+    addSuffix: true,
+  });
 }
