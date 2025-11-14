19f04049c8a6936ec77f4a9d60899397baaf029e
william chavula <rumbani@gmail.com>
2025-11-14 12:35:26 -0600

    feat: implement useCreateOrganization hook for organization creation

---
+++ frontend/app/(main)/employer/onboarding/hooks/use-create-organization.ts
@@ -0,0 +1,47 @@
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+import { instance } from "@/lib/axios-instance";
+
+import { OrganizationWithMembersResponse } from "@/schemas/responses/organizations";
+
+export const useCreateOrganization = () => {
+  const router = useRouter();
+  const {
+    mutateAsync: createOrganizationAsync,
+    isPending: isCreatingOrganization,
+  } = useMutation({
+    mutationFn: async (organizationData: FormData) => {
+      const res = await instance.post<OrganizationWithMembersResponse>(
+        "/organizations",
+        organizationData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+
+      if (!res.data.success || res.status !== 201) {
+        throw new Error("Failed to create organization");
+      }
+
+      return res.data;
+    },
+    onSuccess: async (organizationData: OrganizationWithMembersResponse) => {
+      if (organizationData.success) {
+        toast.success(
+          `Organization "${organizationData.data.name}" created successfully!`,
+        );
+        router.replace(`/employer/organizations/${organizationData.data.id}`);
+      }
+    },
+    onError: (e) => {
+      console.log({ e });
+      toast.error("There was an error creating the organization.");
+    },
+  });
+
+  return { createOrganizationAsync, isCreatingOrganization };
+};
