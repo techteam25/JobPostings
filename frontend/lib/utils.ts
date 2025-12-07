19ac7f5213ff4042979488162ee78ef766b3bea9
william chavula <rumbani@gmail.com>
2025-12-06 22:55:30 -0600

    feat: add formatToRelativeDate utility for relative date formatting

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { formatDistance, subDays, format } from "date-fns";
+import { formatDistance, subDays, format, formatRelative } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -14,4 +14,11 @@ export function formatPostedDate(date: Date) {
 
 export function formatToReadableDate(date: Date) {
   return format(new Date(date), "MMMM dd, yyyy");
+}
+
+export function formatToRelativeDate(date: Date) {
+  // should format relative to now.
+  // After format relative, strip the time component from the returned string
+  const relative = formatRelative(new Date(date), new Date()).split(" at ")[0];
+  return relative[0].toUpperCase() + relative.slice(1);
 }
