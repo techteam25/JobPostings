594c99332c671cbc22e59869b02c5cfcb2016529
william chavula <rumbani@gmail.com>
2025-12-08 12:27:16 -0600

    feat: enhance RadioGroupItem component with customizable props for indicator and circle

--- frontend/components/ui/radio-group.tsx
+++ frontend/components/ui/radio-group.tsx
@@ -20,25 +20,45 @@ const RadioGroup = React.forwardRef<
 });
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 
+type RadioGroupItemProps = React.ComponentPropsWithoutRef<
+  typeof RadioGroupPrimitive.Item
+> & {
+  className?: string;
+  indicatorClassName?: string;
+  circleClassName?: string;
+  children?: React.ReactNode;
+};
+
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, ...props }, ref) => {
-  return (
-    <RadioGroupPrimitive.Item
-      ref={ref}
-      className={cn(
-        "border-primary text-primary focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-        className,
-      )}
-      {...props}
-    >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="fill-accent h-3.5 w-3.5" />
-      </RadioGroupPrimitive.Indicator>
-    </RadioGroupPrimitive.Item>
-  );
-});
+  RadioGroupItemProps
+>(
+  (
+    { className, indicatorClassName, circleClassName, children, ...props },
+    ref,
+  ) => {
+    return (
+      <RadioGroupPrimitive.Item
+        ref={ref}
+        className={cn(
+          "border-primary text-primary focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      >
+        <RadioGroupPrimitive.Indicator
+          className={cn("flex items-center justify-center", indicatorClassName)}
+        >
+          {children ?? (
+            <Circle
+              className={cn("fill-accent h-3.5 w-3.5", circleClassName)}
+            />
+          )}
+        </RadioGroupPrimitive.Indicator>
+      </RadioGroupPrimitive.Item>
+    );
+  },
+);
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
 
 export { RadioGroup, RadioGroupItem };
