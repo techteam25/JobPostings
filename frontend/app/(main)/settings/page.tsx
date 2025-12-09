277da359cc421d6937e6f9e19164a4b9693506f4
william chavula <rumbani@gmail.com>
2025-12-09 15:09:25 -0600

    feat: scaffold new pages for user profile management

--- frontend/app/(main)/settings/page.tsx
+++ frontend/app/(main)/settings/page.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
+
+import { LuConstruction } from "react-icons/lu";
+
 export default function SettingsPage() {
-  return <div>Settings Page</div>;
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <LuConstruction className="size-16 text-amber-400" />
+        </EmptyMedia>
+        <EmptyTitle>Coming Soon</EmptyTitle>
+        <EmptyDescription>
+          Settings page is under construction. Please check back later.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button asChild>
+          <Link href="/">Browse Jobs</Link>
+        </Button>
+      </EmptyContent>
+    </Empty>
+  );
 }
