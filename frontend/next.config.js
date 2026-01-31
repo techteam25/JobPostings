4ff31332d158ca85c3917c317a44874d0be6a2e1
William R. Chavula <60331497+WilliamChavula@users.noreply.github.com>
2026-01-31 16:17:18 -0600

    chore: remove unused webpack configuration from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -25,15 +25,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
